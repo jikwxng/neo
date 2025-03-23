8c22129ed7a83dd232cec8ecda334a8628eee17d
JEEK <jeeks46094@gmail.com>
2025-03-23 22:47:01 +0900

    Update app.js

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.post('/message', async (req, res) => {
     console.log("response:: "+response)
 
     const keywords = JSON.parse(response.split("keyword: ")[1]?.split("option")[0])
-    const options = JSON.parse(response.split("option: ")[1])
+    const options = response.split("option: ")[1] ? JSON.parse(response.split("option: ")[1]) : []
     const content = response.split("keyword: ")[0]?.trim()
 
     console.log("keywords: "+keywords,"options: "+options,"content: "+content)
