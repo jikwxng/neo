021a3d20f2c4e9a2c25d6b318f59cffeae56ded3
JEEK <jeeks46094@gmail.com>
2025-03-24 01:06:37 +0900

    Update app.js

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.post('/message', async (req, res) => {
 
       console.log("response: " + response)
 
-      const keywords = response.split("keyword: [")[1]?.replace("]", "").split(",");
+      const keywords = response.split("keyword: [")[1]?.replaceAll("]", "").replaceAll("\n", "").split(",");
       const options = response.includes("option: [") ? response.split("option: [")[1].split("]")[0].split(",") : null
       const content = response.split("keyword: ")[0]?.trim().replaceAll("[홈페이지]", "https://syhs-h.goeujb.kr/syhs-h/main.do ")
 
